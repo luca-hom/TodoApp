54cf19b300b3f8b3570419402621aba32d3d2df6
luca-hom <luca.homberger@juventus.schule>
2022-04-09 11:10:08 +0200

    added foo components

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -1,11 +1,13 @@
 import { NgModule } from '@angular/core';
 import { BrowserModule } from '@angular/platform-browser';
 
-import { AppComponent } from './app.component';
+import { AppComponent } from './components/root/app.component';
+import { FooComponent } from './components/foo/foo.component';
 
 @NgModule({
   declarations: [
-    AppComponent
+    AppComponent,
+    FooComponent
   ],
   imports: [
     BrowserModule
